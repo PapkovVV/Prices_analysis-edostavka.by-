b2eede4174324dd1357174ae0746d891a6d90cde
Valentine <getgggg2@gmail.com>
2024-04-16 15:48:22 +0300

    Дата цен продуктов вынесена как поле класса. Более ускорен процесс создания аккордиона.

--- KursovayaDB/Views/Pages/ProductsPage.xaml.cs
+++ KursovayaDB/Views/Pages/ProductsPage.xaml.cs
@@ -13,6 +13,7 @@ public partial class ProductsPage : Page
 {
     private bool isUserClick = false;
 
+    DateTime? pricesDate = null;
     List<Category> uniqueCategories;
     List<ProductName> uniqueProducts;
     List<ProductAttribute> uniqueAttributes;
@@ -35,7 +36,7 @@ public partial class ProductsPage : Page
         }
         finally
         {
-            CreateAccordion(DateTime.Now.Date);
+            CreateAccordion();
         }
     }
 
@@ -55,13 +56,8 @@ public partial class ProductsPage : Page
         attributeValues = await getAllAttributeValuesTask;
         productPrices = await getAllPricesTask;
     }
-    private void CreateAccordion(DateTime? pricesDate = null)//Создание аккордиона
+    private void CreateAccordion()//Создание аккордиона
     {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        pricesDate = pricesDate ?? DateTime.Now.Date;
-
         var accordionItemsCollection = accordion.Items;//Получаем коллкцию элементов 
 
         foreach (var category in uniqueCategories.OrderBy(x => x.Id))
@@ -73,55 +69,57 @@ public partial class ProductsPage : Page
                 IsExpanded = false
             };
 
-            List<SimpleAccordionItem> products = new List<SimpleAccordionItem>();//Список элементов продуктов
-            List<string> neededArticles = productPrices.Where(x => x.PriceDate.Date == pricesDate?.Date).Select(x => x.ProductId).ToList();//Артикулы продуктов для отображения
-            List<ProductName> neededProducts = uniqueProducts.Where(x => x.CategoryId == category.Id && neededArticles.Contains(x.Article)).ToList();//Необходимые продукты
-
-            foreach (var product in neededProducts)
-            {
-                var productItem = new SimpleAccordionItem
-                {
-                    Header = product.Name,
-                    HeaderBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ebebf5")),
-                    HeaderBackgroundOnMouseOver = System.Windows.Media.Brushes.White,
-                    HeaderBackgroundOnExpanded = System.Windows.Media.Brushes.White,
-                    HeaderBackgroundOnMouseOverOnExpanded = System.Windows.Media.Brushes.White,
-                    Margin = new Thickness(5),
-                    IsExpanded = false,
-                };
-
-                var productPrice = productPrices.Where(x => x.PriceDate.Date == pricesDate?.Date).FirstOrDefault(x => x.ProductId.Equals(product.Article));//Объект цены продукта
-
-                List<string> attribute_value = new List<string>();
-
-                if (productPrice != null)
-                {
-                    foreach (var attributeValue in attributeValues.Where(x => x.ProductId.Equals(product.Article)))
-                    {
-                        var attr = uniqueAttributes.FirstOrDefault(x => x.Id == attributeValue.AttributeId).Name;
-                        attribute_value.Add($"{attr}: {attributeValue.Value}");
-                    }
-
-                    attribute_value.Add($"Цена за 1 кг: {productPrice.Price} руб.");
-
-                    productItem.Content = new ItemsControl()
-                    {
-                        ItemsSource = attribute_value
-                    };
-                    products.Add(productItem);
-                }
-
-            }
             catItem.Content = new ItemsControl()
             {
-                ItemsSource = products
+                ItemsSource = CreateProductsAccordionItems(category.Id)
             };
 
             accordionItemsCollection.Add(catItem);
         }
+    }
 
-        stopwatch.Stop();
-        MessageBox.Show(stopwatch.ElapsedMilliseconds + "");
+    private List<SimpleAccordionItem> CreateProductsAccordionItems(int categoryId)
+    {
+        pricesDate = pricesDate ?? DateTime.Now.Date;
+        List<SimpleAccordionItem> products = new List<SimpleAccordionItem>();//Список элементов продуктов
+        List<string> neededArticles = productPrices.Where(x => x.PriceDate.Date == pricesDate?.Date).Select(x => x.ProductId).ToList();//Артикулы продуктов для отображения
+        List<ProductName> neededProducts = uniqueProducts.Where(x => x.CategoryId == categoryId && neededArticles.Contains(x.Article)).ToList();//Необходимые продукты
+
+        foreach (var product in neededProducts)
+        {
+            var productItem = new SimpleAccordionItem
+            {
+                Header = product.Name,
+                HeaderBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ebebf5")),
+                HeaderBackgroundOnMouseOver = System.Windows.Media.Brushes.White,
+                HeaderBackgroundOnExpanded = System.Windows.Media.Brushes.White,
+                HeaderBackgroundOnMouseOverOnExpanded = System.Windows.Media.Brushes.White,
+                Margin = new Thickness(5),
+                IsExpanded = false,
+            };
+
+            var productPrice = productPrices.Where(x => x.PriceDate.Date == pricesDate?.Date).FirstOrDefault(x => x.ProductId.Equals(product.Article));//Объект цены продукта
+
+            List<string> attribute_value = new List<string>();
+
+            if (productPrice != null)
+            {
+                foreach (var attributeValue in attributeValues.Where(x => x.ProductId.Equals(product.Article)))
+                {
+                    var attr = uniqueAttributes.FirstOrDefault(x => x.Id == attributeValue.AttributeId).Name;
+                    attribute_value.Add($"{attr}: {attributeValue.Value}");
+                }
+
+                attribute_value.Add($"Цена за 1 кг: {productPrice.Price} руб.");
+
+                productItem.Content = new ItemsControl()
+                {
+                    ItemsSource = attribute_value
+                };
+                products.Add(productItem);
+            }
+        }
+        return products;
     }
 
     private async Task GenerateCategoriesCombo()//Генерация элементов ComboBox категорий (Оптимизировано)
@@ -176,11 +174,11 @@ public partial class ProductsPage : Page
             var selectedItem = pricesDatesCombo.SelectedItem as ComboBoxItemPlus;
             if (selectedItem != null)
             {
-                DateTime neededDate = DateTime.ParseExact(selectedItem.Content.ToString().Trim(), "dd MMMM yyyy", CultureInfo.CurrentCulture);
+                pricesDate = DateTime.ParseExact(selectedItem.Content.ToString().Trim(), "dd MMMM yyyy", CultureInfo.CurrentCulture);
                 if (accordion.HasItems)
                 {
                     accordion.Items.Clear();
-                    CreateAccordion(neededDate);
+                    CreateAccordion();
                 }
             }
         }
